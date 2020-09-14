4e4cb74afa5c31dbc2eb3a30126a1aa8b89f2cb9
Fer Vittorello <fvittorello@gmail.com>
2020-09-14 17:00:21 -0300

    feat(example-03) add new excercise base folders

---
+++ src/components/example-03/App/App.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import './App.css';
+
+const api_key = '6b2b22be';
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './components/example-01/App/App';
-import App from './components/example-02/App/App';
+// import App from './components/example-02/App/App';
+import App from './components/example-03/App/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
