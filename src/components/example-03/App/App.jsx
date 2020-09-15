b290b4085f8a0539e2e56693f8a17614425164e8
Fer Vittorello <fvittorello@gmail.com>
2020-09-15 14:28:16 -0300

    chore(exampla-03/App) add keys to the project

--- src/components/example-03/App/App.jsx
+++ src/components/example-03/App/App.jsx
@@ -2,3 +2,16 @@ import React from 'react';
 import './App.css';
 
 const api_key = '6b2b22be';
+const app_id = '96ca863a0bdf2e850114ba0293136806';
+
+const example = `https://api.edamam.com/search?q=chicken&app_id=${app_id}&app_key=${api_key}`;
+
+const App = () => {
+	return (
+		<div className='App'>
+			<h1>Hello Example 03</h1>
+		</div>
+	);
+};
+
+export default App;
