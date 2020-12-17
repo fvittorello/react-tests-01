cae039eb2b73298b8c408440e298422b0275bfea
Fer Vittorello <fvittorello@gmail.com>
2020-12-17 01:05:49 -0300

    chore() create 480px and below responsive media query rules

--- src/components/example-02/Form/Form.jsx
+++ src/components/example-02/Form/Form.jsx
@@ -17,10 +17,12 @@ export const Form = ({ setInputText, todos, setTodos, inputText, setStatus }) =>
 
 	return (
 		<form>
-			<input value={inputText} onChange={inputTextHandler} type='text' className='todo-input' />
-			<button onClick={submitTodoHandler} type='submit' className='todo-button'>
-				<i className='fas fa-plus-square'></i>
-			</button>
+			<div className='input-container'>
+				<input value={inputText} onChange={inputTextHandler} type='text' className='todo-input' />
+				<button onClick={submitTodoHandler} type='submit' className='todo-button'>
+					<i className='fas fa-plus-square'></i>
+				</button>
+			</div>
 			<div className='select'>
 				<select onChange={statusHandler} name='todos' className='filter-todo'>
 					<option value='all'>All</option>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from 'components/example-01/App/App';
-// import App from 'components/example-02/App/App';
+// import App from 'components/example-01/App/App';
+import App from 'components/example-02/App/App';
 // import App from 'components/example-03/App';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.register();
